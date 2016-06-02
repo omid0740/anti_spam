d919fb4ebc726985e4a0c38e3916b4ff9bd77994
omid0740 <omid0740@gmail.com>
2016-06-02 18:34:59 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {38551457},--Sudo users
+    sudo_users = {223745862},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
